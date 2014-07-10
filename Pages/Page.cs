ab3c5e6545864f5eae9988ce653e4b270f25829d
Bzbuckha <Brian.Buckhana@orasi.com>
2014-07-10 14:21:48 -0400

    OpenURL added to Page object.

--- Pages/Page.cs
+++ Pages/Page.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using OpenQA.Selenium;
-//using OpenQA.Selenium.Firefox;
-//using OpenQA.Selenium.Chrome;
-//using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
 //using OpenQA.Selenium.Support.UI;
 
 namespace BlueSource_Selenium_Project.Pages
@@ -29,5 +29,25 @@ namespace BlueSource_Selenium_Project.Pages
         /***************************************************
          *  FUNCTIONS
          ***************************************************/
+        protected void openURL(String url, object driver, String browserName)
+        {
+            switch (browserName.ToUpper())
+            {
+                case "FIREFOX":
+                    FirefoxDriver fireFoxDriver = (FirefoxDriver) driver;
+                    fireFoxDriver.Navigate().GoToUrl(url);
+                    break;
+                case "CHROME":
+                    ChromeDriver chromeDriver = (ChromeDriver) driver;
+                    chromeDriver.Navigate().GoToUrl(url);
+                    break;
+                default: // Internet Explorer
+                    InternetExplorerDriver internetExplorerDriver = (InternetExplorerDriver) driver;
+                    internetExplorerDriver.Navigate().GoToUrl(url);
+                    break;
+            }
+            
+        }
+
     }
 }
